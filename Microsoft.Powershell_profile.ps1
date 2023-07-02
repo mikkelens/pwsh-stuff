90f263eca225aa777b29165f67af3782d3f4b11a
Mikkel <Mikkel123sej@gmail.com>
2023-07-03 00:37:17 +0200

    put in pc settings

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,22 +1,30 @@
 # appearaance
-oh-my-posh init pwsh --config "C:\Users\mikke\Documents\PowerShell\catppuccin_frappe.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "C:\Users\Mikkel\Documents\PowerShell\catppuccin_frappe.omp.json" | Invoke-Expression
 
-# // git related //
-function cd-git { cd "C:\Users\mikke\Desktop\repos\" }
+# git related
+function cd-git { Set-Location "F:\Git\" }
 New-Alias cdgit cd-git
 
-# // rust related //
+# PC ADDITIONS:
+Set-Variable ToolPath -Option Constant -Value "F:\Tools\CLI"
+function ToolFolder {
+	Start $ToolPath
+}
+function Build-Install-Binary {
+	cargo build --release
+	cargo install .
+}
+
 # docs
 function rust-book { rustup docs --book }
-# function rust-book { Start "C:\Users\mikke\Desktop\Git\Rust\the-book\book\index.html" }
 New-Alias rustbook rust-book
 # directories
-function cd-rust { cd "C:\Users\mikke\Desktop\repos\rust\" }
-function cd-rust-projects { cd "C:\Users\mikke\Desktop\repos\rust\rust-projects\" }
+function cd-rust { Set-Location "F:\Git\Rust\" }
+function cd-rust-projects { Set-Location "F:\Git\Rust\rust-projects\" }
 New-Alias cdrust cd-rust
 New-Alias cdrustprojects cd-rust-projects
 # applications
-function clion { Start "C:\Users\mikke\AppData\Local\JetBrains\Toolbox\apps\CLion\ch-0\222.3345.126\bin\clion64.exe" }
+function clion { Start-Process "C:\Users\mikke\AppData\Local\JetBrains\Toolbox\apps\CLion\ch-0\222.3345.126\bin\clion64.exe" }
 function clion-here { clion . }
 New-Alias clionhere clion-here
 New-Alias clion clion-here
