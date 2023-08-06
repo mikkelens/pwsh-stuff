4e6bf9fe9f6f91f3d23c7255bfd567a709d6e860
Mikkel <Mikkel123sej@gmail.com>
2023-08-06 15:54:53 +0200

    fixed paths

--- local_paths.ps1
+++ local_paths.ps1
@@ -5,5 +5,6 @@ Set-Variable ExternalDrivePath -Option Constant -Value 'F:'
 Set-Variable GitPath -Option Constant -Value ($ExternalDrivePath + '\Git')
 Set-Variable RustPath -Option Constant -Value ($GitPath + '\Rust')
 
-Set-Variable ToolPath -Option Constant -Value ($ExternalDrivePath + '\Tools\CLI')
-Set-Variable WorkspaceBonusSamplePath -Option Constant -Value ($ToolPath + '\bonus.code-workspace')
+Set-Variable ToolPath -Option Constant -Value ($ExternalDrivePath + '\Tools')
+Set-Variable CLIPath -Option Constant -Value ($ToolPath + '\CLI')
+Set-Variable WorkspaceBonusSamplePath -Option Constant -Value ($CLIPath + '\bonus.code-workspace')
