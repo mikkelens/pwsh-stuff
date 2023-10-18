222aa7eaa94d272e9035880d7b7360c4c8e5fc71
Mikkel <Mikkel123sej@gmail.com>
2023-10-18 16:22:51 +0200

    manual PC fix

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -5,15 +5,12 @@ Set-Variable PSPath -Option Constant -Value ($UserPath + '\Documents\PowerShell'
 Set-Variable OMPPath -Option Constant -Value ($PSPath + '\catppuccin_frappe.omp.json')
 oh-my-posh init pwsh --config $OMPPath | Invoke-Expression
 
-. ($PSPath + '\local_paths.ps1') # 'dot scope' shell script variable scope tweak
-. ($PSPath + '\rust_completions.ps1')
-
-Set-Variable GlazeWMFolder -Option Constant -Value ($UserPath + '\.glaze-wm')
+Set-Variable GlazeWMPath -Option Constant -Value ($LocalUserPath + '\.glaze-wm')
 function Open-CLIPath {
 	Start-Process $CLIPath
 }
 function Start-GlazeWM {
-	Start-Process ($ToolPath + '\GlazeWM_x64_1.11.1')
+	Start-Process ($ToolPath + '\GlazeWM_x64_2.0.3')
 }
 New-Alias bib Build-Install-Binary
 function Build-Install-Binary ($specifiedPath) {
--- local_paths.ps1
+++ local_paths.ps1
@@ -1,9 +1,9 @@
-Set-Variable DesktopPath -Option Constant -Value ($UserPath + '\Desktop')
-
+Set-Variable LocalUserPath -Option Constant -Value 'C:\Users\mikke'
 Set-Variable ExternalDrivePath -Option Constant -Value 'F:'
-Set-Variable GitPath -Option Constant -Value ($ExternalDrivePath + '\Git')
-Set-Variable RustPath -Option Constant -Value ($GitPath + '\Rust')
 
-Set-Variable ToolPath -Option Constant -Value ($ExternalDrivePath + '\Tools')
-Set-Variable CLIPath -Option Constant -Value ($ToolPath + '\CLI')
+Set-Variable GitPath -Option Constant -Value ($ExternalDrivePath + '\repos')
+Set-Variable RustPath -Option Constant -Value ($GitPath + '\rust')
+
+Set-Variable ToolPath -Option Constant -Value ($ExternalDrivePath + '\tools')
+Set-Variable CLIPath -Option Constant -Value ($ToolPath + '\cli')
 Set-Variable WorkspaceBonusSamplePath -Option Constant -Value ($CLIPath + '\bonus.code-workspace')
